e62b1d427fd76c09a5773baef67d48783acac9cb
Shivam Shrivastav <shivam23711@gmail.com>
2025-05-25 14:14:19 +0530

    Learning Low Level Design

---
+++ src/main/java/com/lld/LowLevelDesignApplication.java
@@ -0,0 +1,13 @@
+package com.lld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowLevelDesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lld/LowLevelDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
